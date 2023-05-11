090f127ab8d790d842a816cb6b1acf1df52f7128
Sergey Donyk <doncarlos@i.ua>
2023-05-12 00:21:09 +0300

    hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { FeedbackOptions } from './feedback/Feedback/FeadbackOptions';
 import { Statistics } from './feedback/Statistics/Statistics';
 import { Notification } from './feedback/Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = ({ good, neutral, bad }) => {
+  const [total, setTotal] = useState(good + neutral + bad);
+  const [goodValue, setGood] = useState(good);
+  const [neutralValue, setNeutral] = useState(neutral);
+  const [badValue, setBad] = useState(bad);
+  const positiveFeedbackPercentage = Math.round((goodValue / total) * 100) || 0;
+  const handleFeedback = option => {
+    if (option === 'good') {
+      setGood(prevGood => prevGood + 1);
+    } else if (option === 'neutral') {
+      setNeutral(prevNeutral => prevNeutral + 1);
+    } else if (option === 'bad') {
+      setBad(prevBad => prevBad + 1);
+    }
+    setTotal(prevTotal => prevTotal + 1);
   };
 
-  handleFeedback = option => {
-    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalFeedback = good + neutral + bad;
-    const positiveFeedbackPercentage =
-      Math.round((good / totalFeedback) * 100) || 0;
-
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <h1>Please leave only positive reviews &#129488;</h1>
-        <FeedbackOptions
-          options={['good', 'neutral', 'bad']}
-          onLeaveFeedback={this.handleFeedback}
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <h1>Please leave only positive reviews &#129488;</h1>
+      <FeedbackOptions
+        options={['good', 'neutral', 'bad']}
+        onLeaveFeedback={handleFeedback}
+      />
+      {total > 0 ? (
+        <Statistics
+          good={goodValue}
+          neutral={neutralValue}
+          bad={badValue}
+          total={total}
+          positivePercentage={positiveFeedbackPercentage}
         />
-        {totalFeedback > 0 ? (
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={totalFeedback}
-            positivePercentage={positiveFeedbackPercentage}
-          />
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </div>
-    );
-  }
-}
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </div>
+  );
+};
--- src/components/feedback/Statistics/Statistics.js
+++ src/components/feedback/Statistics/Statistics.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
-export const Statistics = ({
-  good,
-  neutral,
-  bad,
-  total,
-  positivePercentage,
-}) => {
+
+export const Statistics = ({ good, neutral, bad }) => {
+  const [total, setTotal] = useState(good + neutral + bad);
+  const positivePercentage = useMemo(() => {
+    return total === 0 ? 0 : Math.round((good / total) * 100);
+  }, [good, total]);
+
   return (
     <div className={css.block}>
       <span>
@@ -27,10 +27,11 @@ export const Statistics = ({
     </div>
   );
 };
+
 Statistics.propTypes = {
   good: PropTypes.number.isRequired,
   neutral: PropTypes.number.isRequired,
   bad: PropTypes.number.isRequired,
-  total: PropTypes.number.isRequired,
-  positivePercentage: PropTypes.number.isRequired,
 };
+
+// export default Statistics;
